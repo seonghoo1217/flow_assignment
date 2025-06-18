3264c80b9a861aae7f2369967f3db26785dd9a0a
seonghoo1217 <lsh6451217@gmail.com>
2025-06-19 04:40:32 +0900

    feat : Application 가동시 더미 데이터 저장 로직 추가
    - ApplicationRunner를 이용하여 애플리케이션 가동시 Default Extension 저장

---
+++ src/main/java/assignment/flow/infra/intialize/FixedExtensionLoader.java
@@ -0,0 +1,27 @@
+package assignment.flow.infra.intialize;
+
+import assignment.flow.application.BlockExtensionCommandService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+@Log4j2
+public class FixedExtensionLoader implements ApplicationRunner {
+
+    private final BlockExtensionCommandService blockExtensionCommandService;
+    private final ExtensionProperties extensionProperties;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        log.info(">>> default extensions: {}", extensionProperties.getDefaults());
+
+        List<String> defaults = extensionProperties.getDefaults();
+        blockExtensionCommandService.initDefaultExtensions(defaults);
+    }
+}
